f90250bfa151bc4a0a9efde57eb3878c2c1c4966
BadTripp <renato.turetta00@gmail.com>
2022-03-23 21:21:32 +0100

    Quadrato loop js complete

--- js/quadratoLoop.js
+++ js/quadratoLoop.js
@@ -1,12 +1,3 @@
-
-function allunga(x,y) {
-    document.getElementById("prova-"+y).style.width = x+"px";
-  }
-var c=1;
-var back=c;
-var giri=0;
-
-
 schermoy=document.getElementById("schermo").clientHeight;
 schermox=document.getElementById("schermo").clientWidth;
 var step=1;
@@ -41,19 +32,4 @@ function move (direzione){
             quadrato.style.top=step+"px"; 
             break;
         }
-}
-
-function cliccato(x){
-    if(giri < 3){
-    console.log(giri);
-    document.getElementById("prova-"+back).style.width ="50px";
-    document.getElementById("prova-"+c).style.width ="100px";
-    c+=1;
-    back=c-1;
-    if(c>5){c=1; giri+=1;}
-    if(giri==3){
-        document.getElementById("prova").style.backgroundColor = "red";
-        
-        }
-    }else {document.getElementById("prova-5").style.width ="50px";}
 }
