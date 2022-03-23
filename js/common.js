efa7cbbd61621c0cfd3a690703cde8ee9902ab0b
BadTripp <renato.turetta00@gmail.com>
2022-03-23 15:40:30 +0100

    onClick

--- js/common.js
+++ js/common.js
@@ -2,3 +2,7 @@
 function allunga(x,y) {
     document.getElementById("prova-"+y).style.width = x+"px";
   }
+
+function cliccato(x){
+    document.getElementById("prova-"+x).style.width = x*100+"px";
+}
