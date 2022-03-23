4b3de3858554abe0693c687b9ce37d74f717c624
BadTripp <renato.turetta00@gmail.com>
2022-03-23 16:46:58 +0100

    Numero massimo giri
    Bottone diventa rosso dopo 3 giri

--- js/common.js
+++ js/common.js
@@ -4,13 +4,20 @@ function allunga(x,y) {
   }
 var c=1;
 var back=c;
+var giri=0;
 
 function cliccato(x){
+    if(giri < 3){
+    console.log(giri);
     document.getElementById("prova-"+back).style.width ="50px";
     document.getElementById("prova-"+c).style.width ="100px";
     c+=1;
     back=c-1;
-    if(c>5){
-        c=1;
+    if(c>5){c=1; giri+=1;}
+    if(giri==3){
+        document.getElementById("prova").style.backgroundColor = "red";
+        
+        document.getElementById("prova-5").style.width ="50px";
+        }
     }
 }
