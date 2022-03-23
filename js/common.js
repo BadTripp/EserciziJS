18071d3040bb54b2fad27dc3b54db5708c48befd
BadTripp <renato.turetta00@gmail.com>
2022-03-23 16:09:07 +0100

    Gioco Blocchi

--- js/common.js
+++ js/common.js
@@ -2,7 +2,15 @@
 function allunga(x,y) {
     document.getElementById("prova-"+y).style.width = x+"px";
   }
+var c=1;
+var back=c;
 
 function cliccato(x){
-    document.getElementById("prova-"+x).style.width = x*100+"px";
+    document.getElementById("prova-"+back).style.width ="50px";
+    document.getElementById("prova-"+c).style.width ="100px";
+    c+=1;
+    back=c-1;
+    if(c>5){
+        c=1;
+    }
 }
